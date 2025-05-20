f98fd046a09fed6379a14f87c4c5b34ec51038b5
mailong2401 <gtmailong@gmail.com>
2025-05-20 13:05:52 +0700

    .

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,6 @@
 require("config.lazy")
 
-require('tokyonight').setup({
+require("tokyonight").setup({
   transparent = true,
   styles = {
     sidebars = "transparent",
@@ -8,4 +8,20 @@ require('tokyonight').setup({
   },
 })
 
-vim.cmd [[colorscheme tokyonight]]
+vim.cmd([[colorscheme tokyonight]])
+
+require("toggleterm").setup({
+  size = 20,
+  open_mapping = [[<c-\>]],
+  direction = "float", -- hoặc 'vertical' hoặc 'float'
+  shade_terminals = true,
+  start_in_insert = true,
+  insert_mappings = true,
+})
+
+vim.keymap.set("n", "<leader>rj", function()
+  local filename = vim.fn.expand("%:t")
+  local classname = vim.fn.expand("%:t:r")
+  local cmd = "javac " .. filename .. " && java " .. classname .. " && read"
+  require("toggleterm.terminal").Terminal:new({ cmd = cmd, direction = "float" }):toggle()
+end, { noremap = true, silent = true, desc = "Run Java file" })
